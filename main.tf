0b5d6f2d5d226ddf41115f0b053fa91ec11e18d6
Ashu <ashu.tes>
2025-07-19 21:29:57 +0530

    added Hari RG

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg-dhondu" {
   name     = "rg-dhondu"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg-hari" {
+  name     = "rg-hari"
+  location = "West Europe"
+}
